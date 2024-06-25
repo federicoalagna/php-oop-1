d9fb1ddaa1c5c9a7ee66d03fb121c0b3939d8f9b
federicoalagna <federicoalagna01@gmail.com>
2024-06-25 14:33:22 +0200

    [UPD] FIX dei nuovi metodi, inclusione di Bootstrap

--- index.php
+++ index.php
@@ -4,104 +4,96 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP OOP 1</title>
+  
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<?php
 
-class Actor {
+                <h1 class="text-center mb-4">Movie List</h1>
+                <?php
 
-    // VARIABILI
-    public $firstName;
-    public $lastName;
+                // CLASSE GENERE
+                class Genre {
+                    public $name;
 
+                    public function __construct($name) {
+                        $this->name = $name;
+                    }
 
-    // COSTRUTTORE
-    public function __construct($firstName, $lastName) {
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
-    }
+                    public function getName() {
+                        return $this->name;
+                    }
+                }
 
-    // METODO
-    public function getFullName() {
-        return $this->firstName . ' ' . $this->lastName;
-    }
-}
+                // CLASSE ATTORE
+                class Actor {
+                    public $firstName;
+                    public $lastName;
 
-class Genre {
+                    public function __construct($firstName, $lastName) {
+                        $this->firstName = $firstName;
+                        $this->lastName = $lastName;
+                    }
 
-    // VARIABILI
-    public $name;
+                    public function getFullName() {
+                        return $this->firstName . ' ' . $this->lastName;
+                    }
+                }
 
-    // COSTRUTTORE
-    public function __construct($name) {
-        $this->name = $name;
-    }
+                // CLASSE MOVIE
+                class Movie {
+                    public $title;
+                    public $director;
+                    public $releaseYear;
+                    private $genres = [];
+                    private $actors = [];
 
-    // METODO
-    public function getName() {
-        return $this->name;
-    }
-}
+                    public static $totalMovies = 0;
 
+                    public function __construct($title, $director, $releaseYear) {
+                        $this->title = $title;
+                        $this->director = $director;
+                        $this->releaseYear = $releaseYear;
+                        self::$totalMovies++;
+                    }
 
-class Movie {
+                    public function setGenres($genres) {
+                        $this->genres = $genres;
+                    }
 
-    // VARIABILI
-    public $title;
-    public $director;
-    public $releaseYear;
-    public $genre;
+                    public function addGenre(Genre $genre) {
+                        $this->genres[] = $genre;
+                    }
 
-    // COSTRUTTORE
-    public function __construct($title, $director, $releaseYear, $genre) {
-        $this->title = $title;
-        $this->director = $director;
-        $this->releaseYear = $releaseYear;
- 
-    }
+                    public function setActors($actors) {
+                        $this->actors = $actors;
+                    }
 
-    public function setGenres($genres) {
-    $this->genre = $genre;
-    }
+                    public function addActor(Actor $actor) {
+                        $this->actors[] = $actor;
+                    }
 
-    public function addGenre(Genre $genre) {
-        $this->genres[] = $genre;
-    }
+                    public function getMovieInfo() {
+                        try {
+                            if (empty($this->title) || empty($this->director) || empty($this->releaseYear)) {
+                                throw new Exception("Alcune informazioni del film sono mancanti.");
+                            }
+                            $genreNames = array_map(fn($genre) => $genre->getName(), $this->genres);
+                            $actorNames = array_map(fn($actor) => $actor->getFullName(), $this->actors);
+                            $genres = implode(', ', $genreNames) ?: 'N/A';
+                            $actors = implode(', ', $actorNames) ?: 'N/A';
 
-    public function setActors($actors) {
-        $this->actors = $actors;
-    }
+                            return "Title: " . $this->title . ", Director: " . $this->director . ", Release Year: " . $this->releaseYear . ", Genres: " . $genres . ", Actors: " . $actors;
+                        } catch (Exception $e) {
+                            return "Errore: " . $e->getMessage();
+                        }
+                    }
 
-    public function addActor(Actor $actor) {
-        $this->actors[] = $actor;
-    }
+                    public static function getTotalMovies() {
+                        return self::$totalMovies;
+                    }
+                }
 
-    // METODO
-    public function getMovieInfo() {
-        try {
-        
-            if(empty($this->title) || empty($this->director) || empty($this->releaseYear) || empty($this->genre)) {
-                throw new Exception("informazioni mancanti.");
-            }
-            return "Title: " . $this->title . ", Director: " . $this->director . ", Release Year: " . $this->releaseYear . ", Genre: " . $this->genre;
-        } catch (Exception $e) {
-            return "Errore: " . $e->getMessage();
-        }
-    }
-}
-
-// OGGETTI ISTANZIATI
-$movie1 = new Movie("Interstellar", "Christopher Nolan", 2014, "Fantascienza");
-$movie2 = new Movie("Fight Club", "David Fincher", 1999, "Azione");
-
-echo $movie1->getMovieInfo() . "<br>";
-echo $movie2->getMovieInfo() . "<br>";
-
-// EXCEPTION 
-$movie3 = new Movie("", "Ari Aster", 2019, "Horror");
-echo $movie3->getMovieInfo() . "<br>";
-?>
+                ?>
 </body>
 </html>
-
